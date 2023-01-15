680fd33f600210dc55a7f6b77a09a32a4ea71fab
Gergana Valcheva <gery@Gerganas-MacBook-Air.local>
2023-01-15 18:06:33 +0200

    updated commit

---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+const quoteContainer = document.getElementById("quote-container");
+const quoterText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+let apiQuotes = [];
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  loader.hidden = true;
+  quoteContainer.hidden = false;
+}
+
+function newQuote() {
+  loading();
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.text.length > 100) {
+    quoterText.classList.add("long-quote");
+  } else {
+    quoterText.classList.remove("long-quote");
+  }
+
+  quoterText.textContent = quote.text;
+  complete();
+}
+
+async function getQuotes() {
+  loading();
+  const apiUrl = "https://type.fit/api/quotes";
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    getQuotes();
+  }
+}
+
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoterText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+newQuoteBtn.addEventListener("click", newQuote);
+twitterBtn.addEventListener("click", tweetQuote);
+
+getQuotes();
